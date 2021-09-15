f40e3a8b09369c25f887232814ff75fb166e3607
hq-devops-admin <hq-devops-admin@fortinet.com>
2021-09-15 16:55:23 -0700

    init/update fortianalyzer galaxy collection:0.0.1

---
+++ plugins/modules/faz_cli_system_logforward_logfieldexclusion.py
@@ -0,0 +1,426 @@
+#!/usr/bin/python
+from __future__ import absolute_import, division, print_function
+# Copyright 2021 Fortinet, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'status': ['preview'],
+                    'supported_by': 'community',
+                    'metadata_version': '1.1'}
+
+DOCUMENTATION = '''
+---
+module: faz_cli_system_logforward_logfieldexclusion
+short_description: Log field exclusion configuration.
+description:
+    - This module is able to configure a FortiAnalyzer device.
+    - Examples include all parameters and values which need to be adjusted to data sources before usage.
+
+version_added: "2.11"
+author:
+    - Link Zheng (@chillancezen)
+    - Jie Xue (@JieX19)
+    - Frank Shen (@fshen01)
+    - Hongbin Lu (@fgtdev-hblu)
+notes:
+    - To create or update an object, use state present directive.
+    - To delete an object, use state absent directive.
+    - Normally, running one module can fail when a non-zero rc is returned. you can also override
+      the conditions to fail or succeed with parameters rc_failed and rc_succeeded
+
+options:
+    enable_log:
+        description: Enable/Disable logging for task
+        required: false
+        type: bool
+        default: false
+    proposed_method:
+        description: The overridden method for the underlying Json RPC request
+        required: false
+        type: str
+        choices:
+          - update
+          - set
+          - add
+    bypass_validation:
+        description: only set to True when module schema diffs with FortiAnalyzer API structure, module continues to execute without validating parameters
+        required: false
+        type: bool
+        default: false
+    state:
+        description: the directive to create, update or delete an object
+        type: str
+        required: true
+        choices:
+          - present
+          - absent
+    rc_succeeded:
+        description: the rc codes list with which the conditions to succeed will be overriden
+        type: list
+        required: false
+    rc_failed:
+        description: the rc codes list with which the conditions to fail will be overriden
+        type: list
+        required: false
+    log-forward:
+        description: the parameter (log-forward) in requested url
+        type: str
+        required: true
+    cli_system_logforward_logfieldexclusion:
+        description: the top level parameters set
+        required: false
+        type: dict
+        suboptions:
+            dev-type:
+                type: str
+                default: 'FortiGate'
+                description:
+                 - 'Device type.'
+                 - 'FortiGate - FortiGate Device'
+                 - 'FortiManager - FortiManager Device'
+                 - 'Syslog - Syslog Device'
+                 - 'FortiMail - FortiMail Device'
+                 - 'FortiWeb - FortiWeb Device'
+                 - 'FortiCache - FortiCache Device'
+                 - 'FortiAnalyzer - FortiAnalyzer Device'
+                 - 'FortiSandbox - FortiSandbox Device'
+                 - 'FortiDDoS - FortiDDoS Device'
+                 - 'FortiNAC - FortiNAC Device'
+                 - 'FortiDeceptor - FortiDeceptor Device'
+                choices:
+                    - 'FortiGate'
+                    - 'FortiManager'
+                    - 'Syslog'
+                    - 'FortiMail'
+                    - 'FortiWeb'
+                    - 'FortiCache'
+                    - 'FortiAnalyzer'
+                    - 'FortiSandbox'
+                    - 'FortiDDoS'
+                    - 'FortiNAC'
+                    - 'FortiDeceptor'
+                    - 'FortiADC'
+                    - 'FortiFirewall'
+            field-list:
+                type: str
+                description: 'List of fields to be excluded.'
+            id:
+                type: int
+                default: 0
+                description: 'Log field exclusion ID.'
+            log-type:
+                type: str
+                default: 'traffic'
+                description:
+                 - 'Log type.'
+                 - 'app-ctrl - Application Control'
+                 - 'appevent - APPEVENT'
+                 - 'attack - Attack'
+                 - 'content - DLP Archive'
+                 - 'dlp - Data Leak Prevention'
+                 - 'emailfilter - Email Filter'
+                 - 'event - Event'
+                 - 'generic - Generic'
+                 - 'history - Mail Statistics'
+                 - 'traffic - Traffic'
+                 - 'virus - Virus'
+                 - 'voip - VoIP'
+                 - 'webfilter - Web Filter'
+                 - 'netscan - Network Scan'
+                 - 'waf - WAF'
+                 - 'gtp - GTP'
+                 - 'dns - Domain Name System'
+                 - 'ssh - SSH'
+                 - 'ssl - SSL'
+                 - 'file-filter - FFLT'
+                 - 'Asset - Asset'
+                 - 'protocol - PROTOCOL'
+                 - 'ANY-TYPE - Any log type'
+                choices:
+                    - 'app-ctrl'
+                    - 'appevent'
+                    - 'attack'
+                    - 'content'
+                    - 'dlp'
+                    - 'emailfilter'
+                    - 'event'
+                    - 'generic'
+                    - 'history'
+                    - 'traffic'
+                    - 'virus'
+                    - 'voip'
+                    - 'webfilter'
+                    - 'netscan'
+                    - 'waf'
+                    - 'gtp'
+                    - 'dns'
+                    - 'ssh'
+                    - 'ssl'
+                    - 'file-filter'
+                    - 'Asset'
+                    - 'protocol'
+                    - 'ANY-TYPE'
+
+'''
+
+EXAMPLES = '''
+ - hosts: fortianalyzer-inventory
+   collections:
+     - fortinet.fortianalyzer
+   connection: httpapi
+   vars:
+      ansible_httpapi_use_ssl: True
+      ansible_httpapi_validate_certs: False
+      ansible_httpapi_port: 443
+   tasks:
+    - name: Log field exclusion configuration.
+      faz_cli_system_logforward_logfieldexclusion:
+         bypass_validation: False
+         rc_succeeded: [0, -2, -3, ...]
+         rc_failed: [-2, -3, ...]
+         log-forward: <your own value>
+         state: <value in [present, absent]>
+         cli_system_logforward_logfieldexclusion:
+            dev-type: <value in [FortiGate, FortiManager, Syslog, ...]>
+            field-list: <value of string>
+            id: <value of integer>
+            log-type: <value in [app-ctrl, appevent, attack, ...]>
+
+'''
+
+RETURN = '''
+request_url:
+    description: The full url requested
+    returned: always
+    type: str
+    sample: /sys/login/user
+response_code:
+    description: The status of api request
+    returned: always
+    type: int
+    sample: 0
+response_message:
+    description: The descriptive message of the api response
+    type: str
+    returned: always
+    sample: OK.
+
+'''
+from ansible.module_utils.basic import AnsibleModule
+from ansible.module_utils.connection import Connection
+from ansible_collections.fortinet.fortianalyzer.plugins.module_utils.napi import NAPIManager
+from ansible_collections.fortinet.fortianalyzer.plugins.module_utils.napi import check_parameter_bypass
+
+
+def main():
+    jrpc_urls = [
+        '/cli/global/system/log-forward/{log-forward}/log-field-exclusion'
+    ]
+
+    perobject_jrpc_urls = [
+        '/cli/global/system/log-forward/{log-forward}/log-field-exclusion/{log-field-exclusion}'
+    ]
+
+    url_params = ['log-forward']
+    module_primary_key = 'id'
+    module_arg_spec = {
+        'enable_log': {
+            'type': 'bool',
+            'required': False,
+            'default': False
+        },
+        'proposed_method': {
+            'type': 'str',
+            'required': False,
+            'choices': [
+                'set',
+                'update',
+                'add'
+            ]
+        },
+        'bypass_validation': {
+            'type': 'bool',
+            'required': False,
+            'default': False
+        },
+        'rc_succeeded': {
+            'required': False,
+            'type': 'list'
+        },
+        'rc_failed': {
+            'required': False,
+            'type': 'list'
+        },
+        'state': {
+            'type': 'str',
+            'required': True,
+            'choices': [
+                'present',
+                'absent'
+            ]
+        },
+        'log-forward': {
+            'required': True,
+            'type': 'str'
+        },
+        'cli_system_logforward_logfieldexclusion': {
+            'required': False,
+            'type': 'dict',
+            'revision': {
+                '6.2.1': True,
+                '6.2.2': True,
+                '6.2.3': True,
+                '6.2.5': True,
+                '6.2.6': True,
+                '6.4.1': True,
+                '6.4.2': True,
+                '6.4.3': True,
+                '6.4.4': True,
+                '6.4.5': True,
+                '7.0.0': True
+            },
+            'options': {
+                'dev-type': {
+                    'required': False,
+                    'revision': {
+                        '6.2.1': True,
+                        '6.2.2': True,
+                        '6.2.3': True,
+                        '6.2.5': True,
+                        '6.2.6': True,
+                        '6.4.1': True,
+                        '6.4.2': True,
+                        '6.4.3': True,
+                        '6.4.4': True,
+                        '6.4.5': True,
+                        '7.0.0': True
+                    },
+                    'choices': [
+                        'FortiGate',
+                        'FortiManager',
+                        'Syslog',
+                        'FortiMail',
+                        'FortiWeb',
+                        'FortiCache',
+                        'FortiAnalyzer',
+                        'FortiSandbox',
+                        'FortiDDoS',
+                        'FortiNAC',
+                        'FortiDeceptor',
+                        'FortiADC',
+                        'FortiFirewall'
+                    ],
+                    'type': 'str'
+                },
+                'field-list': {
+                    'required': False,
+                    'revision': {
+                        '6.2.1': True,
+                        '6.2.2': True,
+                        '6.2.3': True,
+                        '6.2.5': True,
+                        '6.2.6': True,
+                        '6.4.1': True,
+                        '6.4.2': True,
+                        '6.4.3': True,
+                        '6.4.4': True,
+                        '6.4.5': True,
+                        '7.0.0': True
+                    },
+                    'type': 'str'
+                },
+                'id': {
+                    'required': True,
+                    'revision': {
+                        '6.2.1': True,
+                        '6.2.2': True,
+                        '6.2.3': True,
+                        '6.2.5': True,
+                        '6.2.6': True,
+                        '6.4.1': True,
+                        '6.4.2': True,
+                        '6.4.3': True,
+                        '6.4.4': True,
+                        '6.4.5': True,
+                        '7.0.0': True
+                    },
+                    'type': 'int'
+                },
+                'log-type': {
+                    'required': False,
+                    'revision': {
+                        '6.2.1': True,
+                        '6.2.2': True,
+                        '6.2.3': True,
+                        '6.2.5': True,
+                        '6.2.6': True,
+                        '6.4.1': True,
+                        '6.4.2': True,
+                        '6.4.3': True,
+                        '6.4.4': True,
+                        '6.4.5': True,
+                        '7.0.0': True
+                    },
+                    'choices': [
+                        'app-ctrl',
+                        'appevent',
+                        'attack',
+                        'content',
+                        'dlp',
+                        'emailfilter',
+                        'event',
+                        'generic',
+                        'history',
+                        'traffic',
+                        'virus',
+                        'voip',
+                        'webfilter',
+                        'netscan',
+                        'waf',
+                        'gtp',
+                        'dns',
+                        'ssh',
+                        'ssl',
+                        'file-filter',
+                        'Asset',
+                        'protocol',
+                        'ANY-TYPE'
+                    ],
+                    'type': 'str'
+                }
+            }
+
+        }
+    }
+
+    params_validation_blob = []
+    module = AnsibleModule(argument_spec=check_parameter_bypass(module_arg_spec, 'cli_system_logforward_logfieldexclusion'),
+                           supports_check_mode=False)
+
+    faz = None
+    if module._socket_path:
+        connection = Connection(module._socket_path)
+        connection.set_option('enable_log', module.params['enable_log'] if 'enable_log' in module.params else False)
+        faz = NAPIManager(jrpc_urls, perobject_jrpc_urls, module_primary_key, url_params, module, connection, top_level_schema_name='data')
+        faz.validate_parameters(params_validation_blob)
+        faz.process_curd(argument_specs=module_arg_spec)
+    else:
+        module.fail_json(msg='MUST RUN IN HTTPAPI MODE')
+    module.exit_json(meta=module.params)
+
+
+if __name__ == '__main__':
+    main()
